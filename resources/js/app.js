317f802a5fd146c6408464af22097cf17ca3b5d9
Gian Marco <148767808+GianMarcoPimentel@users.noreply.github.com>
2024-04-09 09:46:06 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import './bootstrap';
+
+// Import our custom CSS
+import '~resources/scss/app.scss'
+
+/* import * as bootstrap from 'bootstrap'; */
+
+
+
+// precompilazione imma
+import.meta.glob([
+    '../img/**'
+    ]);
